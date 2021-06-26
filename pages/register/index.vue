ff8bb9b1c7506d808a514874f5cc850a29f7c25a
Afshar07 <mr.afshar0@gmail.com>
2021-06-26 11:10:01 +0430

    Add label animation and logic

--- pages/register/index.vue
+++ pages/register/index.vue
@@ -6,7 +6,7 @@
     <img :src="logoUrl" alt="Oteacher logo" class="logo mt-5 mb-3" />
     <div class="register-container d-flex flex-column align-items-center">
       <div
-        class="title d-flex align-items-center justify-content-between w-100"
+        class="title d-flex align-items-center justify-content-between w-100 mb-5"
       >
         <h2>ثبت نام در اُتیچر</h2>
         <img
@@ -16,20 +16,34 @@
           @click="redirectHome"
         />
       </div>
-      <div>
-        <form @submit.prevent="onSubmit">
-          <label for="email">Email</label>
-          <input type="email" name="email" v-model="email" />
+      <div class="w-100 d-flex justify-content-center">
+        <form
+          @submit.prevent="onSubmit"
+          class="register-form d-flex flex-column align-items-center w-75"
+        >
+          <label for="email" class="email-label" ref="emailLabel">Email</label>
+          <input
+            type="email"
+            name="email"
+            v-model="email"
+            class="email-input"
+            @focus="emailFocus"
+            @focusout="emailRemoveFocus"
+          />
           <br />
-          <label for="password">Password</label>
+          <label for="password" class="password-label" ref="passwordLabel"
+            >Password</label
+          >
           <input
             type="password"
             name="password"
             v-model="password"
-            @keydown.enter="logToken"
+            class="password-input"
+            @focus="passwordFocus"
+            @focusout="passwordRemoveFocus"
           />
           <br />
-          <button>ثبت نام</button>
+          <button class="align-self-center">ثبت نام</button>
         </form>
       </div>
     </div>
@@ -37,11 +51,10 @@
 </template>
 
 <script>
-import PhoneInput from "../../components/utilities/PhoneInput.vue";
 import MainButton from "../../components/utilities/MainButton.vue";
 
 export default {
-  components: { PhoneInput, MainButton },
+  components: { MainButton },
   data() {
     return {
       logoUrl: require("../../assets/logo-signup.png"),
@@ -55,21 +68,43 @@ export default {
       this.$router.push("/");
     },
     onSubmit() {
+      // Send Data to vuex auth.js and send request for signup
       this.$store
         .dispatch("auth/registerUser", {
           email: this.email,
           password: this.password
         })
         .then(() => {
+          // Send token and refresh token to local storage
           this.$store.dispatch("auth/setTokenLocalStorage");
+          // Push to admin section
           this.$router.push("/admin");
         });
+    },
+    emailFocus() {
+      this.$refs.emailLabel.classList.add("email-focused");
+    },
+    emailRemoveFocus() {
+      if (this.email == "") {
+        this.$refs.emailLabel.classList.remove("email-focused");
+      }
+    },
+    passwordFocus() {
+      this.$refs.passwordLabel.classList.add("password-focused");
+    },
+    passwordRemoveFocus() {
+      if (this.password == "") {
+        this.$refs.passwordLabel.classList.remove("password-focused");
+      }
     }
   }
 };
 </script>
 
 <style scoped>
+input {
+  outline: none;
+}
 .main-container {
   background: url("../../assets/register-background.png") no-repeat 50%;
   background-size: 50rem;
@@ -89,13 +124,31 @@ export default {
 .close-icon {
   cursor: pointer;
 }
-.signup-btn {
-  background-color: #3f9eff;
-  width: 60%;
-  color: #fff;
+.register-form {
+  position: relative;
 }
-.signup-btn:hover {
-  background-color: #0f87ff;
-  transition: all 0.3s;
+.email-label {
+  position: absolute;
+  right: 1rem;
+  top: 0.8rem;
+  transition: all 0.4s;
+}
+.email-input,
+.password-input {
+  width: 100%;
+  height: 3rem;
+  margin-bottom: 1.5rem;
+}
+.password-label {
+  position: absolute;
+  top: 6.7rem;
+  right: 0.8rem;
+  transition: all 0.4s;
+}
+.email-focused {
+  top: -2rem;
+}
+.password-focused {
+  top: 4rem;
 }
 </style>
